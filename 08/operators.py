7ea631e5c94b3725f59436202a5f4778643efc15
Klára Hubínková <klara.hubinkova@volny.cz>
2024-11-26 18:02:25 +0100

    Add sourcecodes for eighth lecture

---
+++ 08/operators.py
@@ -0,0 +1,38 @@
+a = 1
+a = a + 2
+print(a)
+
+b = 1
+b += 2
+print(b)
+
+a = a - 4
+print(a)
+
+b -= 4
+print(b)
+
+a = a * (-6)
+print(a)
+
+b *= (-6)
+print(b)
+
+# Floor division - result is integer
+a = a // 2
+print(a)
+
+b //= 2
+print(b)
+
+# Basic division - result is float
+a = a / 3
+print(a)
+
+b /= 3
+print(b)
+
+# Modulo
+print(9 % 3)
+print(4 % 3)
+print(12 % 10)
