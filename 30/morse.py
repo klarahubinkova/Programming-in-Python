f2d35e276910e6e633b8d825eb174f9913e2d1b2
Klára Hubínková <klara.hubinkova@volny.cz>
2025-05-21 20:41:26 +0200

    Add sourcecodes for the thirtieth lecture

---
+++ 30/morse.py
@@ -0,0 +1,17 @@
+morseova_abeceda = {
+    'A': '.-',     'B': '-...',   'C': '-.-.',
+    'D': '-..',    'E': '.',      'F': '..-.',
+    'G': '--.',    'H': '....',   'I': '..',
+    'J': '.---',   'K': '-.-',    'L': '.-..',
+    'M': '--',     'N': '-.',     'O': '---',
+    'P': '.--.',   'Q': '--.-',   'R': '.-.',
+    'S': '...',    'T': '-',      'U': '..-',
+    'V': '...-',   'W': '.--',    'X': '-..-',
+    'Y': '-.--',   'Z': '--..',   ' ': ''
+}
+
+vstup = input("Zadej vstup bez diakritiky: ").upper()
+
+for pismeno in vstup:
+    if pismeno in morseova_abeceda:
+        print(morseova_abeceda[pismeno], end='/')
